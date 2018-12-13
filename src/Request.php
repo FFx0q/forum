0d7b842a042e4913ea42b5bce48b2b199b60a349
FFx0q <dominikszamburski99@gmail.com>
2018-12-13 22:02:24 +0100

    getting a url

---
+++ src/Request.php
@@ -0,0 +1,15 @@
+<?php
+    namespace App;
+    class Request 
+    {
+        public $url;
+
+        public function __construct() 
+        {
+            $this->url = $_SERVER['REQUEST_URI'];
+        }
+        public function getUrl()
+        {
+            return $this->url;
+        }
+    }
