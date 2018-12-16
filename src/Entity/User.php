d32b8af3b31a5aecf8db917e0ebc341296e635b9
FFx0q <dominikszamburski99@gmail.com>
2018-12-16 14:36:32 +0100

    Add User Entity

---
+++ src/Entity/User.php
@@ -0,0 +1,60 @@
+<?php
+    /**
+     * @Entity @Table(name="users")
+     */
+    class User
+    {
+        /** 
+         * @Id 
+         * @Column(type="integer") 
+         * @GeneratedValue 
+         */
+        protected $pid;
+
+        /** 
+         * @Column(type="string") 
+         */
+        protected $name;
+
+        /** 
+         * @Column(type="string") 
+         */
+        protected $password;
+
+        /**
+         *  @Columnt(type="datetime")
+         */
+        protected $join_date;
+
+
+        public function getName()
+        {
+            return $this->name;
+        }
+
+        public function setName($name)
+        {
+            $this->name = $name;
+        }
+
+        public function getPassword()
+        {
+            return $this->password;
+        }
+
+        public function setPassword($password)
+        {
+            $this->password = $password;
+        }
+
+        public function getJoinDate()
+        {
+            return $this->join_date;
+        }
+
+        public function setJoinDate()
+        {
+            $this->join_date = $date;
+        }
+    }
+?>
