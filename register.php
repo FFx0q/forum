789f383b901e9a4779bf237de3f5b22bb07cf166
FFx0q <dominikszamburski99@gmail.com>
2018-12-22 15:56:41 +0100

    create form validation

--- register.php
+++ register.php
@@ -1,17 +1,25 @@
 <?php 
 	require_once(__DIR__.'/vendor/autoload.php');
+	$user = new App\Entity\User();
+	$date = new DateTime();
+	$controller = new App\Base\Controller();
+	$em = $controller->getManager();
 
 	   if($_SERVER["REQUEST_METHOD"] == "POST")
 	   {
-			$user = new App\Entity\User();
-			$date = new DateTime();
-			$controller = new App\Base\Controller();
-			$em = $controller->getManager();
-
 			$username = $_POST['username'];
-		   	$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-		   	$email = $_POST['email'];
-
+			$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+			$email = $_POST['email'];
+			
+			$query = $em->createQuery("SELECT u.name FROM App\Entity\User as u WHERE u.name = :uname");
+			$query->setParameters([
+				'uname'     => $username
+			]);
+			$login = $query->getResult()[0];
+			if(!empty($login)){
+				\App\Base\Route::redirect('user/register');
+				return;
+			}
 			$user->setName($username);
 			$user->setPassword($password);
 			$user->setEmail($email);
