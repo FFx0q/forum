385c97f0e9120fb19fec15015044cef7aaa839e5
FFx0q <dominikszamburski99@gmail.com>
2019-01-18 15:08:12 +0100

    Improve a indexcontroller

--- login.php
+++ login.php
@@ -1,5 +1,7 @@
 <?php
     require_once(__DIR__.'/vendor/autoload.php');
+    require_once(__DIR__.'/config/app.php');
+    
     session_start();
 
     $controller = new App\Controller\UserController();
--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -11,17 +11,15 @@
     {
         public function index() 
         {
-            $count = $this->getManager()->createQuery("SELECT count(c.id) FROM App\Entity\Forum c")->getResult()[0];
-            for($i = 1; $i <= $count[1]; $i++)
-            {
-                $category[] = $this->getManager()->getRepository(Forum::class)->findBy(['id'=>$i]);
-            }
-            for($i = 0; $i < $count[1]; $i++)
-            {
-                $id = $category[$i][0]->getCategory()->getId().'-'.$category[$i][0]->getCategory()->getTitle();
-                $data[$id][] = $category[$i][0]->getId().'-'.$category[$i][0]->getTitle();
-            }
+            $category = $this->getManager()->getRepository(Forum::class)->findAll();
 
+            foreach($category as $key)
+            {
+                $category = $this->getManager()->getRepository(Forum::class)->findBy(['id'=>$key]);
+                $id = $category[0]->getCategory()->getId().'-'.$category[0]->getCategory()->getTitle();
+                $data[$id][] = $category[0]->getId().'-'.$category[0]->getTitle();
+            }
+      
             return $this->render('index/index.twig',[
                 'data' => $data
             ]);
