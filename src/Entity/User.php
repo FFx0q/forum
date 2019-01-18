aa028cfddc5cb75744a72c847171fda7e5c3d017
FFx0q <dominikszamburski99@gmail.com>
2019-01-18 15:26:45 +0100

    change

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -35,44 +35,93 @@
         private $join_date;
 
 
+
+        /**
+         * Get the value of id
+         */ 
+        public function getId()
+        {
+                return $this->id;
+        }
+
+        /**
+         * Get the value of name
+         */ 
         public function getName()
         {
-            return $this->name;
+                return $this->name;
         }
 
+        /**
+         * Set the value of name
+         *
+         * @return  self
+         */ 
         public function setName($name)
         {
-            $this->name = $name;
+                $this->name = $name;
+
+                return $this;
         }
 
+        /**
+         * Get the value of member_password_hash
+         */ 
         public function getPassword()
         {
-            return $this->password;
+                return $this->member_password_hash;
         }
 
-        public function setPassword($password)
+        /**
+         * Set the value of member_password_hash
+         *
+         * @return  self
+         */ 
+        public function setPassword($member_password_hash)
         {
-            $this->password = $password;
+                $this->member_password_hash = $member_password_hash;
+
+                return $this;
         }
-        
+
+        /**
+         * Get the value of email
+         */ 
         public function getEmail()
         {
-            return $this->email;
+                return $this->email;
         }
 
+        /**
+         * Set the value of email
+         *
+         * @return  self
+         */ 
         public function setEmail($email)
         {
-            $this->email = $email;
+                $this->email = $email;
+
+                return $this;
         }
 
+        /**
+         * Get the value of join_date
+         */ 
         public function getJoinDate()
         {
-            return $this->join_date;
+                return $this->join_date;
         }
 
-        public function setJoinDate($date)
+        /**
+         * Set the value of join_date
+         *
+         * @return  self
+         */ 
+        public function setJoinDate($join_date)
         {
-            $this->join_date = $date;
+                $this->join_date = $join_date;
+
+                return $this;
         }
     }
 ?>
