4bf79e663ab9472ffca140bfb4678f78ee6b6fe0
FFx0q <dominikszamburski99@gmail.com>
2019-01-18 15:26:43 +0100

    change

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -35,28 +35,91 @@
          */
         private $post_date;
 
+        /**
+         * Get the value of id
+         */ 
         public function getId()
         {
-            return $this->pid;
+                return $this->id;
         }
 
+        /**
+         * Get the value of author
+         */ 
+        public function getAuthor()
+        {
+                return $this->author;
+        }
+
+        /**
+         * Set the value of author
+         *
+         * @return  self
+         */ 
+        public function setAuthor($author)
+        {
+                $this->author = $author;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of topic
+         */ 
+        public function getTopic()
+        {
+                return $this->topic;
+        }
+
+        /**
+         * Set the value of topic
+         *
+         * @return  self
+         */ 
+        public function setTopic($topic)
+        {
+                $this->topic = $topic;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of post
+         */ 
         public function getPost()
         {
-            return $this->post;
+                return $this->post;
         }
 
+        /**
+         * Set the value of post
+         *
+         * @return  self
+         */ 
         public function setPost($post)
         {
-            $this->post = $post;
+                $this->post = $post;
+
+                return $this;
         }
 
-        public function getCreated()
+        /**
+         * Get the value of post_date
+         */ 
+        public function getPost_date()
         {
-            return $this->created;
+                return $this->post_date;
         }
 
-        public function setCreated($created)
+        /**
+         * Set the value of post_date
+         *
+         * @return  self
+         */ 
+        public function setPost_date($post_date)
         {
-            $this->created = $created;
+                $this->post_date = $post_date;
+
+                return $this;
         }
     }
