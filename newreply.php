4d416a3a56d6c3a8f63199a4a2bc591570a96111
FFx0q <dominikszamburski99@gmail.com>
2019-01-24 13:23:26 +0100

    Some fixes

--- newreply.php
+++ newreply.php
@@ -2,6 +2,8 @@
     require_once(__DIR__.'/vendor/autoload.php');
     require_once(__DIR__.'/config/app.php');
     
+    session_start();
+
     $date = new DateTime();
 	$controller = new App\Controller\PostController();
     $em = $controller->getManager();
@@ -10,7 +12,8 @@
     {
         $post = isset($_POST['post']) ? $_POST['post'] : " ";
         $tid = isset($_GET['tid']) ? $_GET['tid'] : " ";
-        $uid = 1;
+        $uid = isset($_SESSION['login']) ? (int)explode('-', $_SESSION['login'])[0] : " ";
+
         $data = $date->getTimestamp();
         
         $controller->create($uid, $tid, $post, $data);
--- register.php
+++ register.php
@@ -2,6 +2,8 @@
 	require_once(__DIR__.'/vendor/autoload.php');
 	require_once(__DIR__.'/config/app.php');
 
+	session_start();
+
 	$date = new DateTime();
 	$controller = new App\Controller\UserController();
 	$em = $controller->getManager();
