980a9b564054010f657c9b8546316df41c36f691
FFx0q <dominikszamburski99@gmail.com>
2019-04-08 00:19:56 +0200

    improve code

--- src/Controller/SettingsController.php
+++ src/Controller/SettingsController.php
@@ -3,15 +3,19 @@
 
     use App\Base\Controller;
     use App\Base\View;
+    use App\Base\Router;
     use App\Models\User;
 
     class SettingsController extends Controller 
     {
         public function IndexAction()
         {
-            $id = isset($_SESSION['login']) ? $_SESSION['login'] : 0;
+            if(empty($_SESSION['login']))
+                Router::redirect('/user/login');
+            
+            $id = $_SESSION['login'];
             $data = User::getUserById($id);
-
+            
             return View::render('settings/settings.twig',
             [
                 'data' => isset($data) ? $data : 0
